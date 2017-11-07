830fa7c18cd76452c93b640447cbfab191a28446
Leandro Viturino <leandroviturino.si@gmail.com>
2017-11-07 15:36:05 -0200

    Alterando BD Deploy

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+        /*
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -52,8 +52,8 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
-        /*'mysql' => [
+        ],*/
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
@@ -66,7 +66,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],*/
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
